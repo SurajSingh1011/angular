d66ab51b3f9386d06c74f969ad46bc86d56a484d
SurajGrowIndigo <suraj.singh@growindigo.co.in>
2026-01-30 16:01:49 +0530

    Added Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { ParentComponent } from './parent/parent.component';
+import { ChildComponent } from './child/child.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
+    ParentComponent,
+    ChildComponent,
   ],
   imports: [
     BrowserModule,
